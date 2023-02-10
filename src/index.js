0bc0b3a225a64db8ebbc97ef2b8fc084517f77d5
Lucas Mota <contato.lukamota@gmail.com>
2023-02-09 23:10:05 -0300

    created and tested all routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import router from './routes/todos.js'
 
 const app = express()
 const PORT = 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/todos', router)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at: http://localhost:${PORT}`)
---
+++ src/routes/todos.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
+
+const router = express.Router()
+
+let todos = []
+
+router.get('', (req, res) => {
+  return res.json(todos)
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const todo = todos.find((todo) => todo.id === id)
+  return res.json(todo)
+})
+
+router.post('', (req, res) => {
+  const { title } = req.body
+  const todo = {
+    id: uuidv4(),
+    title,
+    completed: false
+  }
+  todos.push(todo)
+  return res.json(todo)
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const index = todos.findIndex((todo) => todo.id === id)
+  todos[index].completed = !todos[index].completed
+  return res.json(todos[index])
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  todos = todos.filter((todo) => {
+    return todo.id != id
+  })
+
+  return res.json(todos)
+})
+
+export default router
